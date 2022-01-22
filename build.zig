99d597691b73f03ef3c116b8da5cfccb5f749950
Komodo <noreply@komodo.ar>
2022-01-22 19:47:48 -0300

    build: add build script

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const fmt = std.fmt.comptimePrint;
+
+const programs = .{ "cpu", "ram", "net" };
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    inline for (programs) |name| {
+        const exe = b.addExecutable(name, fmt("src/{s}.zig", .{name}));
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
+        exe.install();
+        const build_step = b.step(name, fmt("Build the `{s}` program", .{name}));
+        build_step.dependOn(@ptrCast(*std.build.Step, exe.install_step.?));
+    }
+}
