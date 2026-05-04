ee695b31e9c5354ff01949997a9ee9197b46a00f
Komodo <noreply@komodo.ar>
2026-05-04 13:42:50 -0300

    build: use enums instead of strings for program names

--- build.zig
+++ build.zig
@@ -4,32 +4,38 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const programs = .{ "cpu", "ram", "net", "bspwm", "mpd", "pulseaudio" };
+    const programs = .{ .cpu, .ram, .net, .bspwm, .mpd, .pulseaudio };
     inline for (programs) |name| {
         const exe = b.addExecutable(.{
-            .name = "zitrus-" ++ name,
-            .use_llvm = std.mem.eql(u8, name, "pulseaudio"),
+            .name = "zitrus-" ++ @tagName(name),
+            .use_llvm = switch (name) {
+                .pulseaudio => true,
+                else => false,
+            },
             .root_module = b.createModule(.{
-                .root_source_file = b.path("src/" ++ name ++ ".zig"),
+                .root_source_file = b.path("src/" ++ @tagName(name) ++ ".zig"),
                 .target = target,
                 .optimize = optimize,
-                .imports = if (std.mem.eql(u8, name, "pulseaudio")) blk: {
-                    const translate_c = b.addTranslateC(.{
-                        .root_source_file = b.path("src/pulseaudio.h"),
-                        .target = target,
-                        .optimize = optimize,
-                    });
-                    translate_c.linkSystemLibrary("pulse", .{});
-                    break :blk &.{.{
-                        .name = "pulseaudioc",
-                        .module = translate_c.createModule(),
-                    }};
-                } else &.{},
+                .imports = switch (name) {
+                    .pulseaudio => blk: {
+                        const translate_c = b.addTranslateC(.{
+                            .root_source_file = b.path("src/pulseaudio.h"),
+                            .target = target,
+                            .optimize = optimize,
+                        });
+                        translate_c.linkSystemLibrary("pulse", .{});
+                        break :blk &.{.{
+                            .name = "pulseaudioc",
+                            .module = translate_c.createModule(),
+                        }};
+                    },
+                    else => &.{},
+                },
             }),
         });
         const install = b.addInstallArtifact(exe, .{});
         b.getInstallStep().dependOn(&install.step);
-        const build_step = b.step(name, "Build the `" ++ name ++ "` program");
+        const build_step = b.step(@tagName(name), "Build the `" ++ @tagName(name) ++ "` program");
         build_step.dependOn(&install.step);
     }
 }
